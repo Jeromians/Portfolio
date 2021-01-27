89d24d8a73d06f8b06f48074b9fda627ff998ced
Jeromians <rst.aviary@gmail.com>
2021-01-27 10:55:30 +0700

    2nd commit

---
+++ js/Script.js
@@ -0,0 +1,15 @@
+function opensrch(){
+    console.log("Open Search")
+    document.getElementById("Ovelay").style.display = "block";
+    document.getElementById("searchbox").style.display ="none";
+    document.getElementById("divider").style.display ="none"; 
+   }
+   
+function closeSearch(){
+    console.log("Close Search")
+    document.getElementById("Ovelay").style.display = "none";
+    document.getElementById("searchbox").style.display ="block";
+    document.getElementById("divider").style.display ="block"; 
+   }
+   
+
